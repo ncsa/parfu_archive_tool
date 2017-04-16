1c9f59a93f3688355d44fb2d190e364a33348ba8
Roland Haas <rhaas@ncsa.illinois.edu>
2017-04-16 18:10:59 -0500

    compute powers of 2 rather than using a lookup table

--- parfu_file_list_utils.c
+++ parfu_file_list_utils.c
@@ -896,64 +896,7 @@ int int_power_of(int base, int power){
 }
 
 int int_power_of_2(int arg){
-  switch(arg){
-  case 12:
-    return 4096;
-  case 13:
-    return 8192;
-  case 14:
-    return 16384;
-  case 15:
-    return 32768;
-  case 16:
-    return 65536;
-  case 17:
-    return 131072;
-  case 18:
-    return 262144;
-  case 19:
-    return 524288;
-  case 20:
-    return 1048576;
-  case 21:
-    return 2097152;
-  case 0:
-    return 1;
-  case 1:
-    return 2;
-  case 2:
-    return 4;
-  case 3:
-    return 8;
-  case 4:
-    return 16;
-  case 5:
-    return 32;
-  case 6:
-    return 64;
-  case 7:
-    return 128;
-  case 8:
-    return 256;
-  case 9:
-    return 512;
-  case 10:
-    return 1024;
-  case 11:
-    return 2048;
-  case 22:
-    return 4194304;
-  case 23:
-    return 8388608;
-  case 24:
-    return 16777216;
-  case 25:
-    return 33554432;
-  case 26:
-    return 67108864;
-  default:
-    return int_power_of(2,arg);
-  }    
+  return 1 << arg;
 }
 
 parfu_file_fragment_entry_list_t 

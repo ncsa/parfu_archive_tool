7ea2f7371c90a1e412c7bbba54f8693ca53d01e1
Craig P Steffen <craigsteffen@gmail.com>
2018-07-17 12:18:25 -0500

    bnch test gen: creating gpfs options. not tested

--- benchmark_scripts/gen_FPD_test_script.bash
+++ benchmark_scripts/gen_FPD_test_script.bash
@@ -4,7 +4,7 @@
 # user configuration options
 
 # user sets up variables here (unless they're parsed via command line)
-NODES=2
+NODES=1
 ITERATIONS=3
 STRIPE=8
 RANK_DIVISOR=1      # =1 to fill all CPU threads with ranks, =2 for only half of them
@@ -16,7 +16,7 @@ MYEMAIL="craigsteffen@gmail.com"
 # this will be change to iterate through the data sets
 #DATASET="GW"
 #DATASET="Ar"
-#DATASET="VC"
+DATASET="VC"
 
 # select the code we're testing here
 # this will be changed to iterate through the test codes
@@ -25,13 +25,13 @@ MYEMAIL="craigsteffen@gmail.com"
 #CODE="tar_pigz"
 #CODE="mpitar"
 #CODE="ptar"
-#CODE="parfu"
+CODE="parfu"
 #CODE="ptgz"
 
 # select the system we're on.  
 # typically this is set once per system
-#SYSTEM="wrangler_lustre"
-#SYSTEM="wrangler_gpfs"
+#SYSTEM="wrangler_LL"
+SYSTEM="wrangler_LG"
 #SYSTEM="comet"
 #SYSTEM="stampede2"
 #SYSTEM="jyc_slurm"
@@ -67,22 +67,24 @@ touch $SCRIPT_FILE_NAME
 
 # populate intermediate variables according to what system we're on
 case "$SYSTEM" in
-    "wrangler_lustre")
-	JOB_NAME="FPD_wr_LS"
+    "wrangler_LL")
+	JOB_NAME="FPD_wr_LL"
 	FS="lustre"
 	MANAGER="slurm"
 	RANKS_PER_NODE=24
 	DATADIR='${DATA}'
+	ARCDIR='${DATA}'
 	MYMPIRUN_1="ibrun -n "
 	MYMPIRUN_2=" -o 0"
 	QUEUE_NAME="normal"
 	;;
-    "wrangler_hpfs")
-	JOB_NAME="FPD_wr_HP"
-	FS="hpfs"
+    "wrangler_LG")
+	JOB_NAME="FPD_wr_LG"
+	FS="LG"
 	MANAGER="slurm"
 	RANKS_PER_NODE=24
-	DATADIR='${FLASH}/FP_data'
+	DATADIR='${DATA}'
+	ARCDIR='${FLASH}/FP_data'
 	MYMPIRUN_1="ibrun -n "
 	MYMPIRUN_2=" -o 0"
 	QUEUE_NAME="normal"
@@ -93,6 +95,7 @@ case "$SYSTEM" in
 	RANKS_PER_NODE=24
 	FS="lustre"
 	DATADIR='${SCRATCH}'
+	ARCDIR='${SCRATCH}'       
 	MYMPIRUN_1="ibrun -n "
 	MYMPIRUN_2=" -o 0"
 	;;
@@ -103,6 +106,7 @@ case "$SYSTEM" in
 	FS="lustre"
 	QUEUE_NAME="normal"
 	DATADIR='${SCRATCH}'
+	ARCDIR='${SCRATCH}'
 	MYMPIRUN_1="ibrun -n "
 	MYMPIRUN_2=" -o 0"
 	;;
@@ -112,12 +116,14 @@ case "$SYSTEM" in
 	RANKS_PER_NODE=32
 	FS="lustre"
 	DATADIR='/scratch/staff/csteffen/FPD'
+	ARCDIR='/scratch/staff/csteffen/FPD'
 	MYMPIRUN_1="~jphillip/openmpi/bin/mpirun -n "
 	MYMPIRUN_2=""
 #	QUEUE_NAME="normal"
 	;;
     "bw_moab")
 	DATADIR="/scratch/staff/csteffen/FPD_2018"
+	ARCDIR="/scratch/staff/csteffen/FPD_2018"
 	JOBNAME="FPD_BW_Mo"
 	MANAGER="Moab"
 	RANKS_PER_NODE=32
@@ -133,6 +139,7 @@ case "$SYSTEM" in
 	MYMPIRUN_1="aprun -n "
 	MYMPIRUN_2=" -N $(( ${RANKS_PER_NODE}/${RANK_DIVISOR} )) -d $RANK_DIVISOR "	
 	DATADIR="/scratch/staff/csteffen/FPD"
+	ARCDIR="/scratch/staff/csteffen/FPD"
 	;;
     "bridges")
 	JOB_NAME="FPD_Br"
@@ -142,6 +149,7 @@ case "$SYSTEM" in
 	MYMPIRUN_1="ibrun -n "
 	MYMPIRUN_2=" -o 0"
 	DATADIR='${SCRATCH}/FP_data'
+	ARCDIR='${SCRATCH}/FP_data'
 	;;	
 esac
 
@@ -187,7 +195,7 @@ echo "" >> ${SCRIPT_FILE_NAME}
 
 # variables used for configuration
 # RANKS
-# BASEDIR
+# BASE_ARC_DIR
 # STRIPE
 # JOB_ID_VARIABLE
 RANKS=$(( (NODES*RANKS_PER_NODE)/RANK_DIVISOR ))
@@ -195,7 +203,8 @@ RANKS=$(( (NODES*RANKS_PER_NODE)/RANK_DIVISOR ))
 echo 'RANKS='$RANKS >> ${SCRIPT_FILE_NAME}
 echo "RANKS=${RANKS}" 
 echo 'NODES='$NODES >> ${SCRIPT_FILE_NAME}
-echo "BASEDIR="$DATADIR >> ${SCRIPT_FILE_NAME}
+echo "BASE_ARC_DIR="$ARCDIR >> ${SCRIPT_FILE_NAME}
+echo "BASE_TGT_DIR="$DATADIR >> ${SCRIPT_FILE_NAME}
 EXPANDED_STRIPE=$(printf '%04d' "$STRIPE")
 echo 'STRIPE="'$EXPANDED_STRIPE'"' >> ${SCRIPT_FILE_NAME}
 echo 'DATASET="'${DATASET}'"' >> ${SCRIPT_FILE_NAME}
@@ -228,12 +237,12 @@ echo "" >> ${SCRIPT_FILE_NAME}
 
 # check for directories
 echo 'mkdir -p output_files' >> ${SCRIPT_FILE_NAME}
-echo 'TARGET_DIR="$BASEDIR/${DATASET}_data/"' >> ${SCRIPT_FILE_NAME}
+echo 'TARGET_DIR="$BASE_TGT_DIR/${DATASET}_data/"' >> ${SCRIPT_FILE_NAME}
 echo 'if [ ! -d "${TARGET_DIR}" ]; then' >> ${SCRIPT_FILE_NAME}
 echo '    echo "data target dir ${TARGET_DIR} does not exist!"' >> ${SCRIPT_FILE_NAME}
 echo '    exit'  >> ${SCRIPT_FILE_NAME}
 echo 'fi' >> ${SCRIPT_FILE_NAME}
-echo 'ARCHIVE_DIR="$BASEDIR/arc_${STRIPE}"' >> ${SCRIPT_FILE_NAME}
+echo 'ARCHIVE_DIR="$BASE_ARC_DIR/arc_${STRIPE}"' >> ${SCRIPT_FILE_NAME}
 echo 'if [ ! -d "${ARCHIVE_DIR}" ]; then' >> ${SCRIPT_FILE_NAME}
 echo '    echo "archive dir ${ARCHIVE_DIR} does not exist!"' >> ${SCRIPT_FILE_NAME}
 echo '    exit'  >> ${SCRIPT_FILE_NAME}
@@ -246,10 +255,10 @@ echo '    echo "starting iteration $ITER ranks $RANKS"' >> ${SCRIPT_FILE_NAME}
 echo '    START=`date +%s`' >> ${SCRIPT_FILE_NAME}
 case ${CODE} in
     "parfu")
-	echo '    '$MYMPIRUN_1'${RANKS}'$MYMPIRUN_2' parfu C $ARCHIVE_DIR/prod_'${JOB_ID_NAME}'_${ITER}.pfu $TARGET_DIR &> output_files/out_'${JOB_ID_NAME}'_${ITER}.out 2>&1' >> ${SCRIPT_FILE_NAME}
+      	echo '    '$MYMPIRUN_1'${RANKS}'$MYMPIRUN_2' parfu C $ARCHIVE_DIR/prod_'${JOB_ID_NAME}'_${ITER}.pfu $TARGET_DIR &> output_files/out_'${JOB_ID_NAME}'_${ITER}.out 2>&1' >> ${SCRIPT_FILE_NAME}
 	;;
     "tar")
-	echo '    '$MYMPIRUN_1' 1 tar cf $ARCHIVE_DIR/prod_'${JOB_ID_NAME}'_${ITER}.tar $TARGET_DIR > output_files/out_'${JOB_ID_NAME}'_${ITER}.out 2>&1' >> ${SCRIPT_FILE_NAME}
+ 	echo '    '$MYMPIRUN_1' 1 tar cf $ARCHIVE_DIR/prod_'${JOB_ID_NAME}'_${ITER}.tar $TARGET_DIR > output_files/out_'${JOB_ID_NAME}'_${ITER}.out 2>&1' >> ${SCRIPT_FILE_NAME}
 	;;
     "ptgz")
 	echo '    '$MYMPIRUN_1'$RANKS'$MYMPIRUN_2' ptgz -c -d $TARGET_DIR prod_'${JOB_ID_NAME}'_${ITER} &> output_files/out_'${JOB_ID_NAME}'_${ITER}.out 2>&1' >> ${SCRIPT_FILE_NAME}
